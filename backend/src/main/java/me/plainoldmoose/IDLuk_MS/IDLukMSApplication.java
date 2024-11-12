cbacc9d31f5d47848c9ddfc3b45cdb0bb4265c22
LUQUE_VAN_DER_MERWE <zlac180@live.rhul.ac.uk>
2024-11-12 09:50:11 +0000

    Hello world!

---
+++ backend/src/main/java/me/plainoldmoose/IDLuk_MS/IDLukMSApplication.java
@@ -0,0 +1,13 @@
+package me.plainoldmoose.IDLuk_MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDLukMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IDLukMSApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/me/plainoldmoose/IDLuk_MS/IdLukMsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.plainoldmoose.IDLuk_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdLukMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
