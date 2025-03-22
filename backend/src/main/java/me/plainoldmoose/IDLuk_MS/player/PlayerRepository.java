79acb01b716692fc2e98172a0fa95eba4b2ceef8
ZLAC180 <ZLAC180@live.rhul.ac.uk>
2025-03-22 02:12:27 +0000

    Feat(Repo): Implemented findByName SQL query as Java interface

--- backend/src/main/java/me/plainoldmoose/IDLuk_MS/player/PlayerRepository.java
+++ backend/src/main/java/me/plainoldmoose/IDLuk_MS/player/PlayerRepository.java
@@ -1,4 +1,14 @@
 package me.plainoldmoose.IDLuk_MS.player;
 
-public class PlayerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    @Query("SELECT p FROM Player p WHERE p.name = ?1")
+    Optional<Player> findByName(String name);
 }
